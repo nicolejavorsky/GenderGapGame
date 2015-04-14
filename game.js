0b9bfc7cc06e3c7f776984a21bd95575c3657382
tkalmanoff <trianakalmanoff@dyn-160-39-119-97.secure.barnard.edu>
2015-04-14 14:30:55 -0400

    Added beginning sentence for end page

--- game.js
+++ game.js
@@ -158,7 +158,7 @@ var sceneData = [
     }, 
 
     //18 END
-    { question: "", 
+    { question: "Time to pick your kids up! Hope you enjoyed your day off!"; "Here are some facts about your day: "; " - ,
 
     choices: [
         { answer: "Play Again", next: 1 }
